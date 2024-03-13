6fa7b8c170780dd6b86848ef3dfbb9bfacaf700a
IvanovichIII <119001961+IvanovichIII@users.noreply.github.com>
2024-03-13 20:09:16 +0100

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -21,7 +21,7 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 		return ((char *)s1);
 	while (*s1 != '\0' && n >= i)
 	{
-		if (*s1 == *s2 && ft_strncmp(s1, s2, i) == 0)
+		if (ft_strncmp(s1, s2, i) == 0)
 			return ((char *)s1);
 		s1++;
 		n--;
