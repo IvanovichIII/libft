31582ad0972c8fbfbb95b93226ffcc1a00e9ab68
IvanovichIII <119001961+IvanovichIII@users.noreply.github.com>
2024-03-13 21:46:48 +0100

    Update ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -12,8 +12,5 @@
 
 int	ft_isalpha(int c)
 {
-	if (!((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')))
-		return (0);
-	else
-		return (1);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
