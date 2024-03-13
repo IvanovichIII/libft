af028b6b2fa2dd359d1f8cdf2608d84ca670879a
IvanovichIII <119001961+IvanovichIII@users.noreply.github.com>
2024-03-13 18:59:30 +0100

    Update ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -14,7 +14,7 @@ int	ft_isalnum(char *str)
 {
 	while (*str)
 	{
-		if (!((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z')
+		if (!(((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z'))
 				|| (*str >= '0' && *str <= '9')))
 		{
 			return (0);
