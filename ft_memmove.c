8d1c0789603068602bf2316dd7ba7a420c409029
IvanovichIII <119001961+IvanovichIII@users.noreply.github.com>
2024-03-13 12:51:33 +0100

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -14,5 +14,22 @@
 
 void	*memmove(void *dest, const void *src, size_t n)
 {
+	size_t	i;
 
+	if (dest == src)
+		return (dest);
+	if (dest < src)
+	{
+		ft_memcpy(dest, src, n);
+	}
+	else
+	{
+		i = n;
+		while (i > 0)
+		{
+			((char *)dest)[i - 1] = ((char *)src)[i - 1];
+			i--;
+		}
+	}
+	return (dest);
 }
