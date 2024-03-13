9f373a10c482b8c4fb01f42950f095ec23d7e7a1
IvanovichIII <119001961+IvanovichIII@users.noreply.github.com>
2024-03-13 21:40:08 +0100

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -25,7 +25,7 @@ int	ft_atoi(const char *str)
 	if (*str == '-' || *str == '+')
 		if (*str++ == '-')
 			sign = 1;
-	if (ft_isdigit(*str) == 0)
+	if (ft_isdigit(*str))
 		return (0);
 	while (*str >= '0' && *str <= '9')
 	{
