a3e4044c890ee3086e861c01008270feebdaeb8e
IvanovichIII <119001961+IvanovichIII@users.noreply.github.com>
2024-03-13 13:22:11 +0100

    Update libft.h

--- libft.h
+++ libft.h
@@ -22,7 +22,8 @@ char	*ft_memset(char *str, char c, int n);
 char	*ft_bzero(char *str, int n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-size_t	ft_strlcpy(char *dest, const char *src, size_t n)
+size_t	ft_strlcpy(char *dest, const char *src, size_t n);
+size_t	ft_strlcat(char *s1, char *s2, size_t n);
 char	*ft_toupper(char *str);
 char	*ft_strchr(char *str, int c);
 char	*ft_strrchr(char *str, int c);
