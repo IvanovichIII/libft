dd4a5fa29a366622ec6bb9b03409926c1fab2e76
IvanovichIII <119001961+IvanovichIII@users.noreply.github.com>
2024-03-13 13:27:11 +0100

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -14,12 +14,18 @@
 
 int	ft_memcmp(const void *str1, const void *str2, size_t n)
 {
-	while (n-- > 0)
+	const unsigned char	*s1;
+	const unsigned char	*s2;
+	size_t				i;
+
+	s1 = (const unsigned char *)str1;
+	s2 = (const unsigned char *)str2;
+	i = 0;
+	while (i < n)
 	{
-		if (*str1 != *str2)
-			return (*str1 - *str2);
-		str1++;
-		str2++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
 	return (0);
 }
