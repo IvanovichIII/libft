566863667ad5ce9dcaaa30ae54d167932d333bef
IvanovichIII <119001961+IvanovichIII@users.noreply.github.com>
2024-03-13 12:59:11 +0100

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -13,10 +13,10 @@
 #include <stdlib.h>
 #include "libft.h"
 
-void	*ft_calloc(size_t number, size_t size)
+void	*ft_calloc(size_t num, size_t size)
 {
-	void	*ptr;
+	void	*ptr;e
 	
-	ptr = malloc(number * size);
+	ptr = malloc(num * size);
 	return (ft_bzero(ptr));
 }
