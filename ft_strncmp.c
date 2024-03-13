b70ebea56d47bc051c41a12505d3bd53fe14e2ee
IvanovichIII <119001961+IvanovichIII@users.noreply.github.com>
2024-03-13 20:01:57 +0100

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -15,11 +15,11 @@ int	ft_strncmp(const char *s1, const char *s2, unsigned int n)
 	while ((*s1 != '\0' || *s2 != '\0') && n-- > 0)
 	{
 		if (*s2 == '\0')
-			return (*s1);
+			return ((const unsigned char)*s1);
 		if (*s1 > *s2)
-			return (*s1 - *s2);
+			return ((const unsigned char)*s1 - (const unsigned char)*s2);
 		else if (*s1 < *s2)
-			return (*s1 - *s2);
+			return ((const unsigned char)*s1 - (const unsigned char)*s2);
 		s1++;
 		s2++;
 	}
