f96e90f92a93d0d8de90739fc111a0d486b7d237
IvanovichIII <119001961+IvanovichIII@users.noreply.github.com>
2024-03-13 21:46:34 +0100

    Update ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -10,11 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int	ft_isalnum(int c)
 {
-	if (!(((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-			|| (c >= '0' && c <= '9')))
-		return (0);
-	else
-		return (1);
+	return (ft_isalpha(c) || ft_isdigit(c));
 }
