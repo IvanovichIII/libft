afd1781ce09956ddfd3aacf84298eddc7035d038
IvanovichIII <119001961+IvanovichIII@users.noreply.github.com>
2024-03-13 20:05:18 +0100

    Update libft.h

--- libft.h
+++ libft.h
@@ -29,7 +29,7 @@ int		ft_tolower(int str);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strrchr(const char *str, int c);
 int	  ft_strncmp(const char *s1, const char *s2, size_t c);
-char	*ft_strnstr(char *s1, char *s2, int c);
+char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 void	*ft_calloc(size_t number, size_t size);
 char	*ft_strdup(const char *string);
 int		ft_atoi(char *str);
