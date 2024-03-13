5b7dec451438b0c2d6f5d3a31a2fddebcf4a3c79
Ivan <ivan@DESKTOP-A211F27>
2024-03-13 19:41:42 +0100

    hola

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ivan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 19:04:43 by ivan              #+#    #+#             */
-/*   Updated: 2024/03/13 19:05:47 by ivan             ###   ########.fr       */
+/*   Updated: 2024/03/13 19:41:20 by ivan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t n);
 size_t	ft_strlcat(char *s1, char *s2, size_t n);
-int  	ft_toupper(int str);
-int  	ft_tolower(int str);
+int		ft_toupper(int str);
+int		ft_tolower(int str);
 char	*ft_strchr(char *str, int c);
 char	*ft_strrchr(char *str, int c);
 char	*ft_strncmp(const char *s1, const char *s2, size_t c);
