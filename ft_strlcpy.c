99426b3d7de835509599354c7313d2d15e03e72e
IvanovichIII <119001961+IvanovichIII@users.noreply.github.com>
2024-03-13 19:27:05 +0100

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -21,7 +21,7 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	i = 0;
 	if (size != 0)
 	{
-		while (src[i] != '\0' && i < size -1)
+		while (*src != '\0' && i < size -1)
 		{
 			*dest++ = *src++;
 			i++;
