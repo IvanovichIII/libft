e0caa4158916c66b7df286198ae756873c72dc9a
IvanovichIII <119001961+IvanovichIII@users.noreply.github.com>
2024-03-13 21:50:08 +0100

    Update libft.h

--- libft.h
+++ libft.h
@@ -20,7 +20,7 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_strlen(const char *str);
 void	*ft_memset(void *str, int c, size_t n);
-void	ft_bzero(void *str, int n);
+void	ft_bzero(void *str, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t n);
