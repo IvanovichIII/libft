b764058dcb19389e78fff78c5eb61111c21680e0
Ivan <ivan@DESKTOP-A211F27>
2024-03-14 02:07:24 +0100

    hol

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ivan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 19:04:43 by ivan              #+#    #+#             */
-/*   Updated: 2024/03/14 01:31:53 by ivan             ###   ########.fr       */
+/*   Updated: 2024/03/14 02:07:06 by ivan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,3 +39,4 @@ int		ft_atoi(const char *str);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
