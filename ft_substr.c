fb127483775548707478a05fda55ae0fc1117c94
IvanovichIII <119001961+IvanovichIII@users.noreply.github.com>
2024-03-14 01:05:53 +0100

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -18,9 +18,9 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 
 	if (!s)
 		return (NULL);
-	if (ft_strlen(s) < start)
+	if ((unsigned int)ft_strlen(s) < start)
 		len = 0;
-	if (ft_strlen(s + start) < len)
+	if ((size_t)ft_strlen(s + start) < len)
 		len = ft_strlen(s + start);
 	ptr = (char *)malloc((len + 1) * sizeof(char));
 	if (!ptr)
