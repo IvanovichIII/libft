c178cb54e10b287e1ca7a09e8238d9e8fc7963e4
Ivan <ivan@DESKTOP-A211F27>
2024-03-14 01:41:59 +0100

    hola

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,11 +6,29 @@
 /*   By: ivan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 01:31:14 by ivan              #+#    #+#             */
-/*   Updated: 2024/03/14 01:31:30 by ivan             ###   ########.fr       */
+/*   Updated: 2024/03/14 01:41:43 by ivan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-
+	char	*ptr;
+	if (!s1 || !set)
+		return (NULL);
+	ptr = (char *)malloc((ft_strlen(s1) + 1) * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	while (*s1)
+	{
+		while (*set)
+		{
+			if (*s1 != *set)
+			{
+				*ptr++ = *s1++;
+			}
+		}
+	}
+	return (ptr);
 }
